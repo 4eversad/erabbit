4f4358b6f934c6af48e2371e4b06b20e2e1512bf
4eversad <y1085947752@gmail.com>
2022-09-29 20:19:42 +0800

    入口文件

---
+++ erabbit-client-pc/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'normalize.css'
+import '@/assets/styles/common.less'
+import UI from '@/components/library'
+import '@/mock' 
+
+
+// 挂载仓库挂载路由
+
+createApp(App).use(store).use(router).use(UI).mount('#app')
